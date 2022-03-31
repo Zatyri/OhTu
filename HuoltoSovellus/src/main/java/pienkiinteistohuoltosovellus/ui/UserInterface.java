6a8cdb92a61bd2bd36c9cc8d583f9e9c994639f2
Oskar <gustosk@gmail.com>
2022-03-31 19:21:51 +0300

    added menu

--- HuoltoSovellus/src/main/java/pienkiinteistohuoltosovellus/ui/UserInterface.java
+++ HuoltoSovellus/src/main/java/pienkiinteistohuoltosovellus/ui/UserInterface.java
@@ -1,8 +1,15 @@
 package pienkiinteistohuoltosovellus.ui;
 
+import java.io.File;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class UserInterface extends Application {
@@ -10,6 +17,8 @@ public class UserInterface extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
+    final FileChooser fileChooser = new FileChooser();
     
     private Scene mainScene;
     
@@ -19,12 +28,48 @@ public class UserInterface extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        VBox loginPane = new VBox(10);
-        mainScene = new Scene(loginPane, 300,250);
+    public void start(Stage primaryStage) throws Exception {
         
-        stage.setScene(mainScene);
+        VBox vBoxMenuBar = new VBox(CreateMenuBar());
+        mainScene = new Scene(vBoxMenuBar, 960, 600);
         
-        stage.show();
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(mainScene);
+        
+        primaryStage.show();
+    }
+    
+    private MenuBar CreateMenuBar(){
+        MenuBar menuBar = new MenuBar();       
+        
+        Menu file = new Menu("File");
+        Menu edit = new Menu("Edit");
+        Menu view = new Menu("view");
+        Menu report = new Menu("Report");
+        
+        MenuItem open = new MenuItem("Open");
+        MenuItem create = new MenuItem("Create New");   
+        
+        open.setOnAction((final ActionEvent e) -> {
+            fileChooser.setTitle("Open maintnance file");
+            File document = fileChooser.showOpenDialog(new Stage());
+            if (document != null) {
+                openFile(document);
+            }
+        });
+        
+        file.getItems().add(open);
+        file.getItems().add(create);
+        
+        menuBar.getMenus().add(file);
+        menuBar.getMenus().add(edit);
+        menuBar.getMenus().add(view);
+        menuBar.getMenus().add(report);
+        
+        return menuBar;
+    }
+        
+     private void openFile(File file) {
+
     }
 }
